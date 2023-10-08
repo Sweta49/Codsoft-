d17c34d60eeed8430b987450ef0878dbaf87b9d4
Sweta <swetakarn25@gmail.com>
2023-10-08 13:05:33 +0545

    initial commit

---
+++ Level1_task3_Basic Calculator/script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        const result = eval(currentInput);
+        display.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+        currentInput = '';
+    }
+}
+
+
